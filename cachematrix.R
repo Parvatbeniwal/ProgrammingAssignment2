e34a86fe29ef16c18757b8f5a4c25b351d32f9a4
parvatsingh <parvat.singh@sprinklr.com>
2018-03-29 13:10:10 +0530

    assignment matrix Inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+#Initializing inverse to null 
+     inverse <- NULL
+     
+     setMatrix <- function(y){
+          x <<- y
+          inverse <<- NULL
+     }
+     
+     #Creating a function to get matrix
+     getMatrix <- function() x
+     
+     setInverse <- function(matrixInverse) inverse <<- matrixInverse
+     getInverse <- function() inverse
+     
+     #List of functions   
+     list(setMatrix = setMatrix, getMatrix = getMatrix,
+          setInverse = setInverse,
+          getInverse = getInverse)
 }
 
 
@@ -12,4 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  i <- x$getInverse()
+     if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+     }
+     data <- x$getMatrix()
+     i <- solve(data)
+     x$setInverse(i)
+     i
+
 }
